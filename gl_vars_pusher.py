41713c87b9f04a528c13a4e2cb685eaa77703fb4
alex <alexey.v.gaydukov@yandex.ru>
2023-02-16 11:13:57 +0300

    fixed for cases with "=" in value

--- gl_vars_pusher.py
+++ gl_vars_pusher.py
@@ -11,7 +11,7 @@ def parse_env() -> dict:
                 # Empty line or comments
                 continue
             var = t.split('=')[0]
-            val = t.split('=')[1]
+            val = t.split(t.split('=')[0])[1:]
             result.update({var:val})
             ins_str = '- echo '+var+'=${'+var+'} >> .env\n'
             ins_file.write(ins_str)
